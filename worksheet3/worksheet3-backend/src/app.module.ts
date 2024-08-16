26abc2beff97a67a51b3411859e49a6524917e80
Jack Darlington <100313566+jackdar@users.noreply.github.com>
2024-08-16 23:02:33 +0000

    Configure NEST backend and set up database connection

---
+++ worksheet3/worksheet3-backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+require('dotenv').config()
+
+export const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(DB_URI),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
